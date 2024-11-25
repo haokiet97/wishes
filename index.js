5857b24777418dd01a10131f8226fadcfb5dd8b6
hungtrang20111501 <hungtrang20111501@gmail.com>
2024-11-25 07:29:12 +0700

    feat: add patch, delete method

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const _ = require('lodash');
+
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,26 +12,56 @@ app.use(express.json());
 
 let wishes = [];
 
-app.get("/api/v1/wishes", (req, res) => {
-  res.json(wishes);
+// GET /api/v1/wishes
+app.get('/api/v1/wishes', (req, res) => {
+  res.status(200).json(wishes);
 });
 
-app.post("/api/v1/wishes", (req, res) => {
+// POST /api/v1/wishes
+app.post('/api/v1/wishes', (req, res) => {
   const { content, name } = req.body;
 
   if (!content || !name) {
-    return res.status(400).json({ error: "Content and name are required." });
+    return res.status(400).json({ error: 'Content and name are required.' });
   }
 
   const newWish = {
-    id: wishes.length + 1,
+    id: _.size(wishes.length) + 1,
     content,
     name,
     created_at: new Date(),
   };
 
   wishes.push(newWish);
-  res.status(201).json(newWish);
+  res.status(200).json(newWish);
+});
+
+// PATCH /api/v1/wishes/:id
+app.patch('/api/v1/wishes/:id', (req, res) => {
+  const { id } = req.params;
+  const { content, name } = req.body;
+
+  const wish = _.find(wishes, { id: Number(id) });
+
+  if (!wish) {
+    return res.status(404).json({ error: 'Wish not found' });
+  }
+
+  _.assign(wish, { content, name });
+  res.status(200).json(wish);
+});
+
+// DELETE /api/v1/wishes/:id
+app.delete('/api/v1/wishes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const wish = _.remove(wishes, { id: Number(id) });
+
+  if (_.isEmpty(wish)) {
+    return res.status(404).json({ error: 'Wish not found' });
+  }
+
+  res.status(200).json(wish[0]);
 });
 
 app.listen(port, () => {
