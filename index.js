86aa0e4b8185d1e12bc302a34d20a247401391dd
hungtrang20111501 <hungtrang20111501@gmail.com>
2024-11-27 08:19:05 +0700

    feat(mongoose): remove deprecated config

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
